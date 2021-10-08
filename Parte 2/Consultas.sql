bd60124607ad049e70b7cf9213d42fd2a8da3398
Matias Sanchez Villar <67757088+matias-sanchez-villar@users.noreply.github.com>
2021-10-08 17:10:24 -0300

    Mini mejoras

--- Parte 2/Consultas.sql
+++ Parte 2/Consultas.sql
@@ -1,158 +1,195 @@
 use BluePrint
+go
+
+--Por cada cliente listar razÃ³n social, cuit y nombre del tipo de cliente.
+select RazonSocial, CUIT, tc.Nombre from Clientes c
+inner join TiposCliente tc on tc.ID = c.IDTipo
 
 go
 
---Por cada cliente listar razón social, cuit y nombre del tipo de cliente.
-select C.RazonSocial, C.Cuit, T.Nombre from Clientes C 
-inner join TiposCliente t on C.IDTipo = T.ID
+-- Por cada cliente listar razÃ³n social, cuit y nombre de la ciudad y nombre del
+--paÃ­s. SÃ³lo de aquellos clientes que posean ciudad y paÃ­s.
+select RazonSocial, CUIT, ci.Nombre 'Ciudad', p.Nombre 'Pais' from Clientes c
+inner join Ciudades ci on ci.ID = c.IDCiudad
+inner join Paises p on p.ID = ci.IDPais
 
---Por cada cliente listar razón social, cuit y nombre de la ciudad y nombre del
---país. Sólo de aquellos clientes que posean ciudad y país.
-select Cl.RazonSocial, Cl.Cuit, C.Nombre as Ciudad, P.Nombre as Pais
-from Clientes CL
-inner join Ciudades C on CL.IDCiudad = C.ID
-inner join Paises p on C.IDPais = P.ID
+go
 
---Por cada cliente listar razón social, cuit y nombre de la ciudad y nombre del
---país. Listar también los datos de aquellos clientes que no tengan ciudad relacionada.
-select CL.RazonSocial, CL.Cuit, C.Nombre as Ciudad, P.Nombre as Pais
-from Clientes CL
-left join Ciudades C on CL.IDCiudad = C.ID
-left join Paises P on C.IDPais = P.ID
+--Por cada cliente listar razÃ³n social, cuit y nombre de la ciudad y nombre del
+--paÃ­s. Listar tambiÃ©n los datos de aquellos clientes que no tengan ciudad
+--relacionada.
+select RazonSocial, CUIT, ci.Nombre 'Ciudad', p.Nombre 'Pais' from Clientes c
+left join Ciudades ci on ci.ID = c.IDCiudad
+left join Paises p on p.ID = ci.IDPais
 
---Por cada cliente listar razón social, cuit y nombre de la ciudad y nombre del
---país. Listar también los datos de aquellas ciudades y países que no tengan
+go
+
+--Por cada cliente listar razÃ³n social, cuit y nombre de la ciudad y nombre del
+--paÃ­s. Listar tambiÃ©n los datos de aquellas ciudades y paÃ­ses que no tengan
 --clientes relacionados.
-select c.RazonSocial, c.CUIT, ci.Nombre, p.Nombre from Clientes c
-full join Ciudades ci on ci.ID = c.IDCiudad
-full join Paises p on p.ID = ci.ID
-
---Listar los nombres de las ciudades que no tengan clientes asociados. Listar
---también el nombre del país al que pertenece la ciudad.
-select ci.Nombre, p.Nombre from Clientes c
+select RazonSocial, CUIT, ci.Nombre 'Ciudad', p.Nombre 'Pais' from Clientes c
 right join Ciudades ci on ci.ID = c.IDCiudad
-full join Paises p on p.ID = ci.ID
-where c.IDCiudad is null
+inner join Paises p on p.ID = ci.IDPais
 
---Listar para cada proyecto el nombre del proyecto, el costo, la razón social del
+go
+
+-- Listar los nombres de las ciudades que no tengan clientes asociados. Listar
+--tambiÃ©n el nombre del paÃ­s al que pertenece la ciudad.
+select c.Nombre 'Ciudad', p.Nombre 'Pais' from Ciudades c
+inner join Paises p on p.ID = c.IDPais
+left join Clientes cl on cl.IDCiudad = c.ID
+where cl.ID is null
+
+go
+
+--Listar para cada proyecto el nombre del proyecto, el costo, la razÃ³n social del
 --cliente, el nombre del tipo de cliente y el nombre de la ciudad (si la tiene
 --registrada) de aquellos clientes cuyo tipo de cliente sea 'Extranjero' o
 --'Unicornio'.
-select p.Nombre, p.CostoEstimado, c.RazonSocial, tc.Nombre, ci.Nombre from Proyectos p
-inner join Clientes C on c.ID = p.IDCliente
-inner join TiposCliente tc on tc.ID = c.IDTipo
-inner join Ciudades ci on ci.ID = c.IDCiudad
-where tc.Nombre = 'Extranjero' or tc.Nombre = 'Unicornio'
+select p.Nombre, p.CostoEstimado, c.RazonSocial, t.Nombre from Proyectos p
+inner join Clientes c on c.ID = p.IDCliente
+inner join TiposCliente t on t.ID = c.IDTipo
+where t.Nombre in ('Extranjero', 'Unicornio')
 
---Listar los nombre de los proyectos de aquellos clientes que sean de los países
+go
+
+--Listar los nombre de los proyectos de aquellos clientes que sean de los paÃ­ses
 --'Argentina' o 'Italia'.
-select PR.Nombre from Proyectos PR
-inner join Clientes CL on PR.IDCliente = CL.ID
-inner join Ciudades C on C.ID = CL.IDCiudad
-inner join Paises P on P.ID = C.IDPais
-where P.Nombre = 'Argentina' or P.Nombre = 'Italia'
+select p.Nombre, c.RazonSocial, ci.Nombre, pa.Nombre from Proyectos p
+inner join Clientes c on c.ID = p.IDCliente
+inner join Ciudades ci on ci.ID = c.IDCiudad
+inner join Paises pa on pa.ID = ci.IDPais
+where pa.Nombre in ('Argentina', 'Italia')
 
---Listar para cada módulo el nombre del módulo, el costo estimado del módulo,
---el nombre del proyecto, la descripción del proyecto y el costo estimado del
+go
+
+--Listar para cada mÃ³dulo el nombre del mÃ³dulo, el costo estimado del mÃ³dulo,
+--el nombre del proyecto, la descripciÃ³n del proyecto y el costo estimado del
 --proyecto de todos aquellos proyectos que hayan finalizado.
 select m.Nombre, m.CostoEstimado, p.Nombre, p.Descripcion, p.CostoEstimado from Modulos m
 inner join Proyectos p on p.ID = m.IDProyecto
-where p.FechaFin < getdate()
+where p.FechaFin is not null
 
---Listar los nombres de los módulos y el nombre del proyecto de aquellos
---módulos cuyo tiempo estimado de realización sea de más de 100 horas.
-select M.Nombre, P.Nombre from Modulos M
-inner join Proyectos P on P.ID = M.IDProyecto
-where M.TiempoEstimado > 100
+go
 
---Listar nombres de módulos, nombre del proyecto, descripción y tiempo
---estimado de aquellos módulos cuya fecha estimada de fin sea mayor a la
---fecha real de fin y el costo estimado del proyecto sea mayor a cien mil.
-select m.Nombre, p.Nombre, m.Descripcion, m.TiempoEstimado, m.FechaEstimadaFin, m.FechaFin from Modulos m
+--Listar los nombres de los mÃ³dulos y el nombre del proyecto de aquellos
+--mÃ³dulos cuyo tiempo estimado de realizaciÃ³n sea de mÃ¡s de 100 horas.
+select m.Nombre, p.Nombre from Modulos m
 inner join Proyectos p on p.ID = m.IDProyecto
-where m.TiempoEstimado > 100000 and m.FechaEstimadaFin > m.FechaFin
+where m.TiempoEstimado > 100
 
---Listar nombre de proyectos, sin repetir, que registren módulos que hayan
+go
+
+--Listar nombres de mÃ³dulos, nombre del proyecto, descripciÃ³n y tiempo
+--estimado de aquellos mÃ³dulos cuya fecha estimada de fin sea mayor a la
+--fecha real de fin y el costo estimado del proyecto sea mayor a cien mil.
+select m.Nombre, p.Nombre, p.Descripcion, m.TiempoEstimado from Modulos m
+inner join Proyectos p on p.ID = m.IDProyecto
+where p.CostoEstimado > 100000 and m.FechaEstimadaFin > m.FechaFin
+
+go
+
+--Listar nombre de proyectos, sin repetir, que registren mÃ³dulos que hayan
 --finalizado antes que el tiempo estimado.
-select distinct P.Nombre from Proyectos p
-Inner join Modulos M on M.IDProyecto = P.ID
-where M.FechaEstimadaFin < M.FechaFin
+select distinct p.Nombre from Proyectos p
+inner join Modulos m on m.IDProyecto = p.ID
+where m.FechaFin < m.FechaEstimadaFin
 
---Listar nombre de ciudades, sin repetir, que no registren clientes pero sí
+go
+
+--Listar nombre de ciudades, sin repetir, que no registren clientes pero sÃ­
 --colaboradores.
-select distinct C.Nombre from Ciudades c
+select c.Nombre from Ciudades c
+left join Clientes cl on cl.IDCiudad = c.ID
 inner join Colaboradores co on co.IDCiudad = c.ID
-left join Clientes cl on c.ID =cl.ID
 where cl.IDCiudad is null
 
--- Listar el nombre del proyecto y nombre de módulos de aquellos módulos que
--- contengan la palabra 'login' en su nombre o descripción.
-select P.Nombre, M.Nombre from Proyectos P
-inner join Modulos M on M.IDProyecto = P.ID
-where M.Nombre like '%login%' and M.Descripcion like '%Login%'
+go
+
+--Listar el nombre del proyecto y nombre de mÃ³dulos de aquellos mÃ³dulos que
+--contengan la palabra 'login' en su nombre o descripciÃ³n.
+select p.Nombre, m.Nombre, m.Descripcion from Proyectos p
+inner join Modulos m on m.IDProyecto = p.ID
+where m.Nombre like '%login%' or m.Descripcion like '%login%'
+
+go
 
 --Listar el nombre del proyecto y el nombre y apellido de todos los
---colaboradores que hayan realizado algún tipo de tarea cuyo nombre contenga
---'Programación' o 'Testing'. Ordenarlo por nombre de proyecto de manera
+--colaboradores que hayan realizado algÃºn tipo de tarea cuyo nombre contenga
+--'ProgramaciÃ³n' o 'Testing'. Ordenarlo por nombre de proyecto de manera
 --ascendente.
-select P.Nombre, C.Nombre, C.Apellido from Proyectos P
-inner join Modulos M on M.IDProyecto = P.ID
-inner join Tareas T on T.IDModulo = M.ID
-inner join Colaboraciones CO on CO.IDTarea = T.ID
-inner join Colaboradores C on C.ID = CO.IDColaborador
-inner join TiposTarea TI on T.IDTipo = TI.ID
-where TI.Nombre like '%Programacion%' or TI.Nombre like '%Testing%'
-order by P.Nombre asc
+select p.Nombre, co.Nombre, co.Apellido from Proyectos p
+inner join Modulos m on m.IDProyecto = p.ID
+inner join Tareas t on t.IDModulo = m.ID
+inner join TiposTarea tt on tt.ID = t.IDTipo
+inner join Colaboraciones c on c.IDTarea = t.ID
+inner join Colaboradores co on co.ID = c.IDColaborador
+where tt.Nombre like '%ProgramaciÃ³n%' or tt.Nombre like '%Testing%'
+order by p.Nombre asc
 
---Listar nombre y apellido del colaborador, nombre del módulo, nombre del tipo
---de tarea, precio hora de la colaboración y precio hora base de aquellos
---colaboradores que hayan cobrado su valor hora de colaboración más del 50%
+go
+
+--Listar nombre y apellido del colaborador, nombre del mÃ³dulo, nombre del tipo
+--de tarea, precio hora de la colaboraciÃ³n y precio hora base de aquellos
+--colaboradores que hayan cobrado su valor hora de colaboraciÃ³n mÃ¡s del 50%
 --del valor hora base.
-select c.Nombre, c.Apellido, m.Nombre as 'Nombre Modulo', ti.Nombre as 'Tipo Tarea', co.PrecioHora, ti.PrecioHoraBase from Colaboradores c
+select c.Nombre, c.Apellido, m.Nombre, tt.Nombre, co.PrecioHora, tt.PrecioHoraBase from Colaboradores c
 inner join Colaboraciones co on co.IDColaborador = c.ID
 inner join Tareas t on t.ID = co.IDTarea
-inner join TiposTarea ti on ti.ID = t.IDTipo
+inner join TiposTarea tt on tt.ID = t.IDTipo
 inner join Modulos m on m.ID = t.IDModulo
-where co.PrecioHora > (ti.PrecioHoraBase*1.50)
+where co.PrecioHora > ((tt.PrecioHoraBase *150) / 100)
+
+go
 
 --Listar nombres y apellidos de las tres colaboraciones de colaboradores
---externos que más hayan demorado en realizar alguna tarea cuyo nombre de
---tipo de tarea contenga 'Testing'.
-select top 3 c.Nombre, c.Apellido from colaboradores c
-inner join Colaboraciones co on co.IDColaborador = c.ID
-inner join Tareas t on t.ID = co.IDTarea
-inner join TiposTarea ti on ti.ID = t.IDTipo
-where c.Tipo = 'E' and ti.Nombre like '%Testing%'
-order by co.Tiempo
+--externos que mÃ¡s hayan demorado en realizar alguna tarea cuyo nombre de
+--tipo de tarea contenga 'Testing'
+select top(3) c.Nombre, c.Apellido, tt.Nombre from Colaboradores c
+inner join Colaboraciones cc on cc.IDColaborador = c.ID
+inner join Tareas t on t.ID = cc.IDTarea
+inner join TiposTarea tt on tt.ID = t.IDTipo
+inner join Modulos m on m.ID = t.IDModulo
+where c.Tipo = 'E' and tt.Nombre like '%Testing%'
+
+go
 
 --Listar apellido, nombre y mail de los colaboradores argentinos que sean
 --internos y cuyo mail no contenga '.com'.
-select C.Nombre, C.Apellido, C.EMail from Colaboradores C
-inner join Ciudades CI on CI.ID = C.IDCiudad
-inner join Paises P on P.ID = CI.IDPais
-where C.Tipo = 'I' and C.EMail not like '%.com%' and P.Nombre = 'Argentina'
+select c.Nombre, c.Apellido, c.EMail, p.Nombre from Colaboradores c
+inner join Ciudades ci on ci.ID = c.IDCiudad
+inner join Paises p on p.ID = ci.IDPais
+where p.Nombre = 'Argentina' and c.EMail not like '%.com%'
 
---Listar nombre del proyecto, nombre del módulo y tipo de tarea de aquellas
+
+go
+
+--Listar nombre del proyecto, nombre del mÃ³dulo y tipo de tarea de aquellas
 --tareas realizadas por colaboradores externos.
-select Distinct p.Nombre, m.Nombre, ti.Nombre from Proyectos p
+select p.Nombre, m.Nombre, tt.Nombre from Proyectos p
 inner join Modulos m on m.IDProyecto = p.ID
 inner join Tareas t on t.IDModulo = m.ID
-inner join TiposTarea ti on ti.ID = t.IDTipo
+inner join TiposTarea tt on tt.ID = t.IDTipo
 inner join Colaboraciones c on c.IDTarea = t.ID
 inner join Colaboradores co on co.ID = c.IDColaborador
 where co.Tipo = 'E'
 
+go
+
 --Listar nombre de proyectos que no hayan registrado tareas.
-select P.Nombre from Proyectos P
-inner join Modulos M on M.IDProyecto = P.ID
-left join Tareas T on T.IDModulo = M.ID
-where T.ID is null
+select p.Nombre from Proyectos p
+inner join Modulos m on m.IDProyecto = p.ID
+left join Tareas t on t.IDModulo = M.ID
+where t.ID is null
+
+go
 
 --Listar apellidos y nombres, sin repeticiones, de aquellos colaboradores que
---hayan trabajado en algún proyecto que aún no haya finalizado.
-select distinct C.Apellido + ' ' + C.Nombre as 'Nombre y Apellido' from Colaboradores C
-inner join Colaboraciones CO on CO.IDColaborador = C.ID
-inner join Tareas T on T.ID = CO.IDTarea
-inner join Modulos M on M.ID = T.IDModulo
-inner join Proyectos P on p.ID = M.IDProyecto
-where P.FechaFin is null or p.FechaFin < getdate()
+--hayan trabajado en algÃºn proyecto que aÃºn no haya finalizado
+select distinct c.Nombre, c.Apellido, p.FechaFin from Colaboradores c
+inner join Colaboraciones co on co.IDColaborador = c.ID
+inner join Tareas t on t.ID = co.IDTarea
+inner join TiposTarea tt on tt.ID = t.IDTipo
+inner join Modulos m on m.ID = t.IDModulo
+inner join Proyectos p on p.ID = m.IDProyecto
+where p.FechaFin is null
